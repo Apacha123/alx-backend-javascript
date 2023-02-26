55a0c996e12c0440939733b4e030f691cd2be51f
Apacha123 <apacha336@gmail.com>
2023-02-26 20:57:22 +0300

    add

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,17 +1,62 @@
-/**
- *
- * @param {Number} a - first integer
- * @param {Number} b - second integer
- * @param {String} type - type of operations
- * @returns- Sum of the two integers
- */
-function calculateNumber(type, a, b) {
-    if (type === 'SUM') return Math.round(a) + Math.round(b);
-    if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
-    if (type === 'DIVIDE') {
-        if (Math.round(b) === 0) return 'Error';
-        return Math.round(a) / Math.round(b);
-    }
-}
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
-module.exports = calculateNumber;
+describe('calculateNumber', () => {
+    describe('SUM', () => {
+        it('Should test positive floating number', () => {
+            expect(calculateNumber('SUM', 1.3, 2.5)).to.equal(4);
+        });
+
+        it('Should test negative floating number', () => {
+            expect(calculateNumber('SUM', -1.3, -2.5)).to.equal(-3);
+        });
+
+        it('Should test two decimal point number', () => {
+            expect(calculateNumber('SUM', 1.35, 5.66)).to.equal(7);
+        });
+
+        it('Should test two mixed type', () => {
+            expect(calculateNumber('SUM', '3.5', 5.66)).to.equal(10);
+        });
+    });
+});
+
+describe('SUBTRACT', () => {
+    it('Should test positive floating number', () => {
+        expect(calculateNumber('SUBTRACT', 1.3, 2.5)).to.equal(-2);
+    });
+
+    it('Should test negative floating number', () => {
+        expect(calculateNumber('SUBTRACT', -1.3, -2.5)).to.equal(1);
+    });
+
+    it('Should test two decimal point number', () => {
+        expect(calculateNumber('SUBTRACT', 5.66, 2.56)).to.equal(3);
+    });
+
+    it('Should test two mixed type', () => {
+        expect(calculateNumber('SUBTRACT', '5.66', 2.0)).to.equal(4);
+    });
+});
+
+describe('DIVIDE', () => {
+    it('Should test positive floating number', () => {
+        expect(calculateNumber('DIVIDE', 1.3, 2.5)).to.equal(0.3333333333333333);
+    });
+
+    it('Should test positive floating number', () => {
+        expect(calculateNumber('DIVIDE', 6.4, 2.4)).to.equal(3.0);
+    });
+
+    it('Should test negative floating number', () => {
+        expect(calculateNumber('DIVIDE', -6.4, -2.4)).to.equal(3.0);
+    });
+
+    it('Should test for Zero division', () => {
+        expect(calculateNumber('DIVIDE', 1.3, 0.4)).to.equal('Error');
+    });
+
+    it('Should test for string type', () => {
+        expect(calculateNumber('DIVIDE', 10.6, '2.4')).to.equal(5.5);
+    });
+});
